475ad983a49a44c01d9cb13ca21106098f392eea
killpanda <mayue617@gmail.com>
2012-10-17 14:55:07 +1100

    Change the homepage

--- brainstorm/views.py
+++ brainstorm/views.py
@@ -4,17 +4,15 @@ from django.shortcuts import render_to_response
 from random import randint
 
 def index(request):
+	total_words = KeyWord.objects.all().count()
 	max_num = KeyWord.objects.all().order_by("-id")[0].id
 	random_word_id = randint(1, max_num)
 	random_word = KeyWord.objects.get(id=random_word_id)
-	return render_to_response('index.html', {'random_word': random_word})
+	return render_to_response('index.html', {'random_word': random_word, 'total_words':total_words})
 	
 def showWord(request, key_word_id):
 	key_word = KeyWord.objects.get(id=int(key_word_id))
-<<<<<<< HEAD
-	return render_to_response('index.html', {'random_word': key_word})
-
-
+	return render_to_response('word.html', {'key_word': key_word})
 
 
 
@@ -27,7 +25,4 @@ apple = KeyWord.objects.get(word_name='apple')
 python.related_word.add(apple)
 
 '''
-=======
-	return render_to_response('word.html', {'key_word': key_word})
-	
->>>>>>> master
+
